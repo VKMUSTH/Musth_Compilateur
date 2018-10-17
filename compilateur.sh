14890e61e2a7570a2ffbdacbc65b9f55ba8a4ef0
VKMUSTH <valerykempf@gmail.com>
2018-10-17 20:53:30 +0200

    Create compilateur.sh

---
+++ compilateur.sh
@@ -0,0 +1,54 @@
+#Initialiser la séquence d'installation
+	sudo apt-get update
+
+#0 MALDUINO
+	cd
+	cd Desktop/COMPILATEUR/0.0_malduino
+
+#1 GIT
+	cd
+	cd Desktop/COMPILATEUR/0_git
+	bash git.sh
+
+#2 xdotool
+	cd
+	cd Desktop/COMPILATEUR/1_xdotool
+	bash xdotool.sh
+#3 gedit
+	cd 2_gedit
+	bash gedit.sh
+
+#4 apache2
+	cd
+	cd Desktop/COMPILATEUR/4_apache2
+	bash apache2.sh
+
+#5_php
+	cd
+	cd Desktop/COMPILATEUR/5_php
+	bash php.sh
+
+#6_mysql
+	cd
+	cd Desktop/COMPILATEUR/6_mysql
+	bash mysql.sh
+
+#7_phpmyadmin
+	cd
+	cd Desktop/COMPILATEUR/7_phpmyadmin
+	bash phpmyadmin.sh
+
+#8 arduino
+	cd
+	cd Desktop/COMPILATEUR/8_arduino
+	bash arduino.sh
+
+#9 gimp
+	cd
+	cd Desktop/COMPILATEUR/9_gimp
+	bash gimp.sh
+
+#10 firefox
+	cd
+	cd Desktop/COMPILATEUR/10_firefox
+	bash firefox.sh
