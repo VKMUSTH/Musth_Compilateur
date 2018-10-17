29c47036b0222d85511f887f321a187459a286ba
VKMUSTH <valerykempf@gmail.com>
2018-10-17 21:38:14 +0200

    Create audio.sh

---
+++ audio.sh
@@ -0,0 +1,3 @@
+amixer cset numid=3 0
+
+#Here the output is being set to 2, which is HDMI. Setting the output to 1 switches to analogue (headphone jack). The default setting is 0 which is automatic.
