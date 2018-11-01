0bf4c37093d5fd3188ecb0821a4e61d547727c4f
VKMUSTH <valerykempf@gmail.com>
2018-11-01 12:39:24 +0100

    Update musth.sh

--- musth.sh
+++ musth.sh
@@ -1,4 +1,8 @@
 #todo
+#5 musth so what it  is ?
+	bash importer_musth.sh
+#6 splashpic
+	bash splashpic.sh
 #1 AJOUTER BDD ADMIN À PHPMYADMIN
 #2 AJOUTER L'INDEX DE BASE AVEC ELEMENTS DE CONNEXION
 	bash php_files.sh
