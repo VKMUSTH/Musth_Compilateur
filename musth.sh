71bf22f55ed8285b77f8f826710d3370b4aa996d
VKMUSTH <valerykempf@gmail.com>
2018-11-01 12:57:52 +0100

    Update musth.sh

--- musth.sh
+++ musth.sh
@@ -1,4 +1,8 @@
-#initialiser
+#Initialiser la séquence d'installation
+	sudo apt update
+	sudo apt upgrade
+	sudo apt update
+#Séquence d'installation  
 bash logiciels.sh
 bash configuration.sh
 bash data.sh
