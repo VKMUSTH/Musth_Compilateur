f7c9d0c6041cd74fa0e99e0ff56577a661426437
VKMUSTH <valerykempf@gmail.com>
2018-11-01 12:48:37 +0100

    Update musth.sh

--- musth.sh
+++ musth.sh
@@ -1,16 +1,7 @@
 #todo
 #5 musth so what it  is ?
 	bash importer_musth.sh
-#6 splashpic
-	bash splashpic.sh
+
 #1 AJOUTER BDD ADMIN À PHPMYADMIN
 #2 AJOUTER L'INDEX DE BASE AVEC ELEMENTS DE CONNEXION
 	bash php_files.sh
-#3 SYNCHRONISER HEURE
-	bash synchrniser_heure.sh
-#4 AUDIO
-	bash audio.sh
-#5 WALLPAPER
-#6 SPLASHPIC
-	bash splashpic.sh
-
