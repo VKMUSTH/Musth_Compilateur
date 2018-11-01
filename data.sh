20ba73faeb90fbde764103f0f6f75bdc9eac664c
VKMUSTH <valerykempf@gmail.com>
2018-11-01 12:50:36 +0100

    Update and rename musth.sh to data.sh

---
+++ data.sh
@@ -0,0 +1,7 @@
+#todo
+#1 musth so what it  is ?
+	bash importer_musth.sh
+#2 AJOUTER BDD ADMIN À PHPMYADMIN
+
+#3 AJOUTER L'INDEX DE BASE AVEC ELEMENTS DE CONNEXION
+	bash php_files.sh
--- musth.sh
+++
@@ -1,7 +0,0 @@
-#todo
-#5 musth so what it  is ?
-	bash importer_musth.sh
-
-#1 AJOUTER BDD ADMIN À PHPMYADMIN
-#2 AJOUTER L'INDEX DE BASE AVEC ELEMENTS DE CONNEXION
-	bash php_files.sh
