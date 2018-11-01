2e5f25cc6a4ca8b91ef4f0fb6ceedb59d5ff4500
VKMUSTH <valerykempf@gmail.com>
2018-11-01 12:36:12 +0100

    Update compilateur.sh

--- compilateur.sh
+++ compilateur.sh
@@ -2,37 +2,23 @@
 	sudo apt update
 	sudo apt upgrade
 	sudo apt update
-#se positionner sur le bureau
-	cd Desktop/Musth_Compilateur
-
 #1 apache2
 	bash apache2.sh
 #2 php
 	bash php.sh
-
 #3 mysql
 	bash mysql.sh
-
 #4 phpmyadmin
 	bash phpmyadmin.sh
-	
 #5 musth
 	bash importer_musth.sh
-
 #6 splashpic
 	bash splashpic.sh
-	
 #7 gedit
 	bash gedit.sh
-
 #8 arduino
 	bash arduino.sh
-
 #9 gimp
 	bash gimp.sh
-
 #10 firefox
 	bash firefox.sh
-
-
-
