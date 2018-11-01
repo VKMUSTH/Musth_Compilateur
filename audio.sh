c2db71e573ea17208304046eb0e2da75b57c7b6c
VKMUSTH <valerykempf@gmail.com>
2018-11-01 17:56:52 +0100

    Update audio.sh

--- audio.sh
+++ audio.sh
@@ -1,3 +1,9 @@
-amixer cset numid=3 0
+# audio sur le jack
+sudo amixer cset numid=3 1
 
-#Here the output is being set to 2, which is HDMI. Setting the output to 1 switches to analogue (headphone jack). The default setting is 0 which is automatic.
+# audio sur le HDMI
+#sudo amixer cset numid=3 2
+
+# commutation automatique
+#sudo amixer cset numid=3 0
+
