3c21140fa3f6fd83540130f89d8a2bcf4b3a2e4a
VKMUSTH <valerykempf@gmail.com>
2019-01-04 03:45:28 +0100

    Update phpmyadmin.sh

--- phpmyadmin.sh
+++ phpmyadmin.sh
@@ -1,6 +1,11 @@
 #source: https://raspbian-france.fr/installer-serveur-web-raspberry-lamp/
 #Installer phpmyadmin
 sudo apt install phpmyadmin
+#choisir apache2
+#choisir non à cette question: Faut-il configurer la base de données de phpmyadmin avec dbconfig-common ? 
 
 #Vérifier l'installation de phpmyadmin lien symbolique
 sudo ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin
+
+#supression phpmyadmin
+#sudo apt-get autoremove --purge phpmyadmin
