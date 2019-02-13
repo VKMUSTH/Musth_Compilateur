2727a050690411ee5267197d05866e803fe6d537
VKMUSTH <valerykempf@gmail.com>
2019-02-13 09:52:56 +0100

    Update splashpic.sh

--- splashpic.sh
+++ splashpic.sh
@@ -6,7 +6,10 @@ cd
 cd /usr/share/plymouth/themes/pix
 
 #télécharger l'image
-wget --output-document=splash.png https://user-images.githubusercontent.com/22053566/46971153-b7b65e00-d0aa-11e8-8905-c458eccbfe8e.png
+#dog bootpic
+#get --output-document=splash.png https://user-images.githubusercontent.com/22053566/46971153-b7b65e00-d0aa-11e8-8905-c458eccbfe8e.png
+#ape bootpic
+get --output-document=splash.png https://user-images.githubusercontent.com/22053566/52698711-6349bf00-2f74-11e9-9f56-eb273248737f.png
 
 #Refermer les permissions
 sudo chmod -R 644 /usr/share/plymouth/themes/pix/
