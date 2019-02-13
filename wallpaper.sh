19b0cbece0cb4ee3a5cd0a2091d7bf8b192f9644
VKMUSTH <valerykempf@gmail.com>
2019-02-13 10:04:33 +0100

    Create wallpaper.sh

---
+++ wallpaper.sh
@@ -0,0 +1,15 @@
+#Ouvrir les permissions
+sudo chmod -R 777 /usr/share/plymouth/themes/pix/
+
+#se placer dans le répertoire voulu
+cd
+cd /usr/share/plymouth/themes/pix
+
+#télécharger l'image
+#dog bootpic
+#get --output-document=splash.png https://user-images.githubusercontent.com/22053566/46971153-b7b65e00-d0aa-11e8-8905-c458eccbfe8e.png
+#ape bootpic
+#get --output-document=splash.png https://user-images.githubusercontent.com/22053566/52698711-6349bf00-2f74-11e9-9f56-eb273248737f.png
+
+#Refermer les permissions
+sudo chmod -R 644 /usr/share/plymouth/themes/pix/
