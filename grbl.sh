b4a3ece8dac999f642ccfba98f1108ddee4bf6eb
VKMUSTH <valerykempf@gmail.com>
2019-03-19 15:41:01 +0100

    Update grbl.sh

--- grbl.sh
+++ grbl.sh
@@ -9,3 +9,12 @@ cd /boot
 ls -l cmdline.txt
 sudo nano cmdline.txt
 #ttyAMAO tty
+sudo apt-get update
+sudo apt-get install xrdp
+sudo apt-get libudev-dev
+sudo apt-get install qtcreator
+sudo apt-get install git
+mkdir github
+cd github
+git clone https://github.com/zapmaker/GrblHoming
+curl http://www.shapeoko.com/wiki/images/6/6a/Coaster.txt > Coaster.nc
